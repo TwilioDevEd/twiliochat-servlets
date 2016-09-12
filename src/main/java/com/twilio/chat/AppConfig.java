3b00408dd03bee7935067b7f07c6e15591ccd0f7
Mario Celi <mceli@stackbuilders.com>
2016-09-12 13:39:14 -0500

    Use twilio-java next-gen version

--- src/main/java/com/twilio/chat/AppConfig.java
+++ src/main/java/com/twilio/chat/AppConfig.java
@@ -9,42 +9,40 @@ import java.util.stream.Stream;
 
 public class AppConfig {
 
-    private final Map<EnvironmentVariable, Optional<String>> configurations;
+  private final Map<EnvironmentVariable, Optional<String>> configurations;
 
-    enum EnvironmentVariable {
-        TWILIO_IPM_SERVICE_SID,
-        TWILIO_API_SECRET,
-        TWILIO_API_KEY,
-        TWILIO_ACCOUNT_SID
-    }
+  public AppConfig() {
+    Stream<EnvironmentVariable> stream = Arrays.stream(EnvironmentVariable.values());
+    this.configurations = stream.collect(Collectors.toMap(Function.identity(),
+        v -> Optional.ofNullable(System.getenv(v.toString()))));
+  }
 
-    public AppConfig() {
-        Stream<EnvironmentVariable> stream = Arrays.stream(EnvironmentVariable.values());
-        this.configurations = stream.collect(Collectors.toMap(Function.identity(), v -> Optional.ofNullable(System.getenv(v.toString()))));
-    }
+  String getTwilioIPMServiceSID() {
+    return configurations.get(EnvironmentVariable.TWILIO_IPM_SERVICE_SID).orElse("");
+  }
 
-    String getTwilioIPMServiceSID() {
-        return configurations.get(EnvironmentVariable.TWILIO_IPM_SERVICE_SID).orElse("");
-    }
+  String getTwilioAPISecret() {
+    return configurations.get(EnvironmentVariable.TWILIO_API_SECRET).orElse("");
+  }
 
-    String getTwilioAPISecret() {
-        return configurations.get(EnvironmentVariable.TWILIO_API_SECRET).orElse("");
-    }
+  String getTwilioAPIKey() {
+    return configurations.get(EnvironmentVariable.TWILIO_API_KEY).orElse("");
+  }
 
-    String getTwilioAPIKey() {
-        return configurations.get(EnvironmentVariable.TWILIO_API_KEY).orElse("");
-    }
+  String getTwilioAccountSID() {
+    return configurations.get(EnvironmentVariable.TWILIO_ACCOUNT_SID).orElse("");
+  }
 
-    String getTwilioAccountSID() {
-        return configurations.get(EnvironmentVariable.TWILIO_ACCOUNT_SID).orElse("");
-    }
+  public boolean isIncomplete() {
+    return configurations.values().stream().anyMatch(value -> !value.isPresent());
+  }
 
-    public boolean isIncomplete() {
-        return configurations.values().stream().anyMatch(value -> !value.isPresent());
-    }
+  @Override
+  public String toString() {
+    return configurations.toString();
+  }
 
-    @Override
-    public String toString() {
-        return configurations.toString();
-    }
+  enum EnvironmentVariable {
+    TWILIO_IPM_SERVICE_SID, TWILIO_API_SECRET, TWILIO_API_KEY, TWILIO_ACCOUNT_SID
+  }
 }
